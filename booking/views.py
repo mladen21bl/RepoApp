2475486e00b45f9101942092b1904dcf65eecf39
Mladen <mladenpopovic50@gmail.com>
2023-08-07 13:30:56 +0200

    radi paginacija i load donekle

--- booking/views.py
+++ booking/views.py
@@ -564,7 +564,7 @@ class NekretninaList(ListView):
         context['selected_lift'] = self.request.GET.get('lift', '')
         context['selected_parking'] = self.request.GET.get('parking', '')
         context['selected_klima'] = self.request.GET.get('klima', '')
-        paginator = Paginator(context['lista'], 4)  # Show 4 items per page
+        paginator = Paginator(context['lista'], 4)  
         page_number = self.request.GET.get('page')
         paginated_lista = paginator.get_page(page_number)
 
